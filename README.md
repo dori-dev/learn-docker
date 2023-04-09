d6db504e03f934ff0c47761958a9dc3ed4384ae1
dori-dev <mr.dori.dev@gmail.com>
2023-04-09 18:45:56 +0330

    create app container

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
 docker-compose up --build
 docker-compose down
 docker-compose up -d
-
-export DJANGO_SUPERUSER_PASSWORD=admin
-python manage.py createsuperuser --noinput --user admin --email admin@site.com
--- django/volumes/app/home/views.py
+++ django/volumes/app/home/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('<h1>Hello, Django!<h1>')
+    return HttpResponse(
+        '<h1>Hello, Django!<h1>'
+        "<img src='/media/image.png'>"
+    )
